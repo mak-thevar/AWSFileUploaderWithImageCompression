90de2bd07b38e21c68594b2fd5850afd4be23712
Mak Thevar <mak.thevar11@gmail.com>
2022-06-06 18:57:55 +0530

    Made bucket name optional.

--- AWSFileUploaderWithImageCompression/Interfaces/IS3ImageUploader.cs
+++ AWSFileUploaderWithImageCompression/Interfaces/IS3ImageUploader.cs
@@ -6,6 +6,6 @@ namespace AWSFileUploaderWithImageCompression
     public interface IS3ImageUploader
     {
         Task<PutObjectResponse> UploadAsync(Stream sourceImageStream, string bucketName = "", string key = "", S3StorageClass? storageClass = null);
-        Task<PutObjectResponse> UploadAsync(string sourceImagePath, string bucketName, string key = "", S3StorageClass? storageClass = null);
+        Task<PutObjectResponse> UploadAsync(string sourceImagePath, string bucketName = "", string key = "", S3StorageClass? storageClass = null);
     }
 }
